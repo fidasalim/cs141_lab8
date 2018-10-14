48b9272c77ebb4e08fe4966f62d807745bc7c898
fidasalim <42141530+fidasalim@users.noreply.github.com>
2018-10-14 16:49:58 +0530

    Update lab8_q2.cpp

--- lab8_q2.cpp
+++ lab8_q2.cpp
@@ -38,11 +38,30 @@ int func3(int arr[]){
 	return sum/10;
 
 }
+//using function for median
+void func4(int arr[]){
+	int i,j;
+	//using loop for checking the median
+	for(i=0;i<10;i++){
+		int a = arr[i];
+		for (j=i+1;j<10;j++){
+			if(arr[j]<arr[i]){	
+				a=arr[j];
+				arr[j]=arr[i];
+				arr[i]=a;
+			}
+		}
+		
+	}
+	float m = (float(arr[4])/2)+(float(arr[5])/2);
+		cout<<"The median of the elements in the array is "<<m<<endl;	
+}
 int main(){
 	int arr[10]={1,2,10,11,5,6,3,2,8,4};  
 	//call for functions
 	cout<<"The maximum of the numbers in the array: "<<func1(arr)<<endl;
 	cout<<"The minimum of the numbers in the array: "<<func2(arr)<<endl;
 	cout<<"The mean of the numbers in the array: "<<func3(arr)<<endl;
+	func4(arr);
 	return 0;
 }
